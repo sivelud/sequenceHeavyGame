55bca1ef22a281e50cc80763fb2aa9b242e431b7
Sivert <sivelud@gmail.com>
2023-10-31 19:05:31 +0100

    restruct

--- src/engine.py
+++ src/engine.py
@@ -2,120 +2,145 @@ import copy as cp
 import numpy as np
 
 
-class GameTester():
-    def __init__(self):
-        self.judge = Judge()
+class Player():
+    def __init__(self, id):
+        self.cards = []
+        self.id = id
 
-        self.cards_on_hand = 7
+    def add_card(self, card):
+        self.cards.append(card)
 
-        
-        
+    def play_card(self, card):
+        if card in self.cards:
+            card = self.cards.pop(self.cards.index(card))
+            return card
+        else:
+            return False
     
-    def sim(self):
+    def __str__(self):
+        self.sort_cards()
+        return f"Player {self.id} deck: {', '.join(map(str, self.cards))}"
+    
+    def sort_cards(self):
+        self.cards.sort()
 
-        self.judge.deal_hand(self.cards_on_hand)
-
-        cards = self.judge.return_hand("Player 1")
-
+    def has_card(self, card):
+        if card in self.cards:
+            return 1
+        else:
+            return 0
+        
         
 
-        self.judge.play_card(cards[0], "Player 1")
-
-
-        
-
-        self.judge.print_deck_to_terminal()
-
-
-class Judge():
+class Board():
     def __init__(self):
-        self.board = {
-    "ruter 2": None, "ruter 3": None, "ruter 4": None, "ruter 5": None, "ruter 6": None, "ruter 7": None, "ruter 8": None, "ruter 9": None,
-    "spar ess": None, "kløver 5": None, "kløver 4": None, "kløver 3": None, "kløver 2": None, "hjerter ess": None, "hjerter konge": None, "ruter 10": None,
-    "spar konge": None, "kløver 6": None, "hjerter 5": None, "hjerter 4": None, "hjerter 3": None, "hjerter 2": None, "hjerter dame": None, "ruter dame": None,
-    "spar dame": None, "kløver 7": None, "hjerter 6": None, "hjerter 7": None, "hjerter 8": None, "hjerter 9": None, "hjerter 10": None, "ruter konge": None,
-    "spar 10": None, "kløver 8": None, "kløver 9": None, "kløver 10": None, "kløver dame": None, "kløver konge": None, "kløver ess": None, "ruter ess": None,
-    "spar 9": None, "spar 8": None, "spar 7": None, "spar 6": None, "spar 5": None, "spar 4": None, "spar 3": None, "spar 2": None
-}
-        self.player1_hand = []
-        self.player2_hand = []
+        self.board_dic = {
+            "♦2": None, "♦3": None, "♦4": None, "♦5": None, "♦6": None, "♦7": None, "♦8": None, "♦9": None,
+            "♠A": None, "♣5": None, "♣4": None, "♣3": None, "♣2": None, "♥A": None, "♥K": None, "♦10": None,
+            "♠K": None, "♣6": None, "♥5": None, "♥4": None, "♥3": None, "♥2": None, "♥Q": None, "♦Q": None,
+            "♠Q": None, "♣7": None, "♥6": None, "♥7": None, "♥8": None, "♥9": None, "♥10": None, "♦K": None,
+            "♠10": None, "♣8": None, "♣9": None, "♣10": None, "♣Q": None, "♣K": None, "♣A": None, "♦A": None,
+            "♠9": None, "♠8": None, "♠7": None, "♠6": None, "♠5": None, "♠4": None, "♠3": None, "♠2": None
+        }
+
+    def __str__(self):
+        output = []
+        keys = list(self.board_dic.keys())
+        
+        # Find the maximum length of a tile (key)
+        max_tile_length = max(len(key) for key in keys)
+        max_status_length = max(len(self.get_tile_status(key)) for key in keys)
+        
+        for i in range(0, len(keys), 8):
+            row = keys[i:i+8]
+            
+            # Format the tiles to ensure they all have the same width
+            tiles = ' | '.join([f"{tile:{max_tile_length}}: {self.get_tile_status(tile):{max_status_length}}" for tile in row])
+            
+            output.append(tiles)
+            output.append('-' * len(tiles))  # Separator line
+
+        return '\n'.join(output)
+
+    def get_tile_status(self, tile):
+        """Returns the status of a tile based on its value in board_dic."""
+        if self.board_dic[tile] == None:
+            return ' - '
+        elif self.board_dic[tile] == 1:
+            return '(1)'
+        elif self.board_dic[tile] == 2:
+            return '(2)'
+        else:
+            return '?'
+
+    def place_card(self, card, player):
+        if self.board_dic[card] == None:
+            self.board_dic[card] = player
+            return 1
+        else:
+            return 0
+        
+    def is_free(self, card):
+        if self.board_dic[card] == None:
+            return 1
+        else:
+            return 0
+    
+
+class Deck():
+    def __init__(self):
+        self.cards_list = [
+            "♠2", "♠3", "♠4", "♠5", "♠6", "♠7", "♠8", "♠9", "♠10", "♠J", "♠Q", "♠K", "♠A",
+            "♦2", "♦3", "♦4", "♦5", "♦6", "♦7", "♦8", "♦9", "♦10", "♦J", "♦Q", "♦K", "♦A",
+            "♣2", "♣3", "♣4", "♣5", "♣6", "♣7", "♣8", "♣9", "♣10", "♣J", "♣Q", "♣K", "♣A",
+            "♥2", "♥3", "♥4", "♥5", "♥6", "♥7", "♥8", "♥9", "♥10", "♥J", "♥Q", "♥K", "♥A"
+        ]
+        self.cards_list_without_J = [
+            "♠2", "♠3", "♠4", "♠5", "♠6", "♠7", "♠8", "♠9", "♠10", "♠Q", "♠K", "♠A",
+            "♦2", "♦3", "♦4", "♦5", "♦6", "♦7", "♦8", "♦9", "♦10", "♦Q", "♦K", "♦A",
+            "♣2", "♣3", "♣4", "♣5", "♣6", "♣7", "♣8", "♣9", "♣10", "♣Q", "♣K", "♣A",
+            "♥2", "♥3", "♥4", "♥5", "♥6", "♥7", "♥8", "♥9", "♥10", "♥Q", "♥K", "♥A"
+        ]
         self.deck = []
         self.reset_deck()
 
-    def return_hand(self, player):
-        if player == "Player 1":
-            return self.player1_hand
-        elif player == "Player 2":
-            return self.player2_hand
-        
-        return None
-
-        
-    def deal_hand(self,n_cards):
-        for i in range(0,n_cards):
-            card = self.deck.pop()
-            self.player1_hand.append(card)
-            card = self.deck.pop()
-            self.player2_hand.append(card)
-
-
-    def print_player_hand(self):
-        print("\nPlayer 1 hand:")
-        for elem in self.player1_hand:
-            print(elem)
-
-        print("\nPlayer 2 hand:")
-        for elem in self.player2_hand:
-            print(elem)
-
-            
-
+    def __str__(self):
+        return str(self.deck)
+    
     def reset_deck(self):
-        copy_board = cp.deepcopy(self.board)
-        for key in copy_board.keys():
-            self.deck.append(key)
+        self.deck = cp.deepcopy(self.cards_list_without_J)
         np.random.shuffle(self.deck)
 
+    def draw_card_fom_deck(self):
+        return self.deck.pop()
 
-    def print_deck_to_terminal(self):
-        tiles = cp.deepcopy(self.board)
-        i = 0
-        printer = ""
-        for tile in tiles:
-            printer += tile + ": "
-            if self.board[tile]:
-                printer += "["
-                printer += str(self.board[tile])
-                printer += "]"
+
+class Sequense_game():
+    def __init__(self):
+        self.board = Board()
+        self.deck = Deck()
+        self.player1 = Player(1)
+        self.player2 = Player(2)
+        self.player = {1: self.player1, 2: self.player2}
+
+    def deal_hands(self,n_cards):
+        for i in range(0,n_cards):
+            self.player1.add_card(self.deck.draw_card_fom_deck())
+            self.player2.add_card(self.deck.draw_card_fom_deck())
+
+    def play_card(self, player, card):
+        if not (player == 1 or player == 2):
+            print("ERROR: Player must be 1 or 2")
+            return 0
+        if self.player[player].has_card(card):
+            if self.board.is_free(card):
+                self.board.place_card(card, player)
+                self.player[player].play_card(card)
+                return 1
             else:
-                printer += "[FREE]"
-            i += 1
-            if i == 8:
-                print(printer)
-                i = 0
-                printer = ""
-
-    # def remove_card_from_player(self, player, card):
-
-        
-
-
-    def play_card(self, card, player):
-        if player == "Player 1":
-            if card in self.player1_hand:
-                self.player1_hand.pop(card)
-
-        elif player == "Player 2":
-            if card in self.player2_hand:
-                self.player1_hand.pop(card)
-
+                print("ERROR: Cant play card. Already pin in that spot")
+                return 0
         else:
-            print("player", player, "cant play card: ", card)
-            return
-        
-        
-        
-        self.board[card] == str(player)
-        
-
-    
+            print("ERROR: Player dont have the card", card)
+            return 0
+                
--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
-from engine import *
+from engine import Sequense_game
 
 
-tester = GameTester()
-tester.sim()
+game = Sequense_game()
+game.deal_hands(7)
+print(game.player1)
+
+
+card = game.player1.cards[0]
+
+# print(game.play_card(1, card))
+game.play_card(1, card)
+print(game.board)
