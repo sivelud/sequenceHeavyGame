a52cb0cc91332604ce1d0dff72a1a82605cb7dee
Sivert <sivelud@gmail.com>
2023-11-01 03:47:03 +0100

    FUNKER men ser stygt ut og mangler win condition 2

--- src/game_src/engine.py
+++ src/game_src/engine.py
@@ -185,10 +185,14 @@ class Sequense_game_1_player(Sequense_game):
 
     def play_then_bot_play(self, player, card):
         out = self.play_card(player, card)
+        if self.check_if_won():
+            print("Player", self.check_if_won(), "won!")
         # if not self.check_if_won():
         #     self.bot_player_2_plays_card()
         # return out
         self.bot_player_2_plays_card()
+        if self.check_if_won():
+            print("Player", self.check_if_won(), "won!")
         return out
 
 
